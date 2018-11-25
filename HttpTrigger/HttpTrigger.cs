c6e6f9aa4f66254fd932b1dc70ede4681e9ac1ae
Mark Gossa <mark@gossa.co.uk>
2018-11-25 13:47:51 +0000

    Add project files.

---
+++ HttpTrigger/HttpTrigger.cs
@@ -0,0 +1,28 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace HttpTrigger
+{
+    public static class HttpTrigger
+    {
+        [FunctionName("HttpTrigger")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+            ActionResult result = new OkObjectResult($"Hello {data.FirstName} {data.LastName}");
+            return result;
+        }
+    }
+}
